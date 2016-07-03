85f18ab18d0ece000c14c3aa44bc346c185ea9c1
zaroursamy <samy.zarour8430@gmail.com>
2016-07-03 16:53:22 +0200

    premier commit

---
+++ src/Chapitre1.scala
@@ -0,0 +1,27 @@
+/**
+  * Created by Samy on 02/07/2016.
+  */
+object Chapitre1 {
+
+  def main(args: Array[String]) {
+    var capital = Map("US"->"Washington", "France"->"Paris")
+    capital += ("Japan"->"Tokyo")
+    println(capital("France"))
+
+    def factorial(x: BigInt):BigInt =
+      if(x==0) 1 else x*factorial(x-1)
+
+    println(factorial(30))
+
+    import java.math.BigInteger
+
+    def factorial2(x: BigInteger): BigInteger=
+      if(x== BigInteger.ZERO) BigInteger.ONE else x.multiply(factorial2(x.subtract(BigInteger.ONE)))
+
+    println(factorial2(BigInteger.TEN))
+
+
+  }
+
+
+}
