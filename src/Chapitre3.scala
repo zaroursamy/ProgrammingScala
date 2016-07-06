abb90ab7820fbf6bbc731cd54b16a9fbb9d7984b
zaroursamy <samy.zarour8430@gmail.com>
2016-07-07 00:22:26 +0200

    p84 chap3

--- src/Chapitre3.scala
+++ src/Chapitre3.scala
@@ -1,3 +1,5 @@
+import java.rmi.activation.UnknownObjectException
+
 /**
   * Created by Samy on 06/07/2016.
   */
@@ -7,6 +9,7 @@ object Chapitre3 {
     val big2 = big*big
     println(big2)
 
+    // on ne peut pas reassigner greetStrings, mais on peut modifier ses elements .
     val greetStrings = new Array[String](3)
     greetStrings(0) = "hey"
     greetStrings(1) = " "
@@ -15,5 +18,14 @@ object Chapitre3 {
     greetStrings.foreach(print)
     for(i <- greetStrings) print(i)
     for(i <- 0 to greetStrings.length-1) print(greetStrings(i))
+
+    // les méthodes de classe sans parentheses pour un parametre
+    Console println 10
+    val unString = "un string"
+    println(unString.charAt(1))
+    println(unString charAt 1)
+
   }
+
+
 }
