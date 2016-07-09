90d1be19c639a8532f79a2ec20c7981f779effba
zaroursamy <samy.zarour8430@gmail.com>
2016-07-09 20:37:57 +0200

    p87 listes

--- src/Chapitre3.scala
+++ src/Chapitre3.scala
@@ -25,6 +25,22 @@ object Chapitre3 {
     println(unString.charAt(1))
     println(unString charAt 1)
 
+    // scala comprend que c'est un array de string. chaque fois qu'il y a une (), une methode apply() est appelée
+    val numNames = Array("zero","one","two")
+    val numNames2 = Array.apply("zero","one","two")
+
+    // les arrays sont mutables mais pas les listes
+    val uneListe = List(1,2,3)
+
+    // concaténation de liste
+    val unDeux = List(1,2)
+    val troisQuatre = List(3,4)
+    val unDeuxTroisQuatre = unDeux ::: troisQuatre
+    // cons ( :: operator)
+    val unDeuxTrois = 1 :: List(2,3)
+
+
+
   }
 
 
