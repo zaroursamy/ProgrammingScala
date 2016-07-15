f184543130ac9ecb61c1408b557ddd206b2a32de
zaroursamy <samy.zarour8430@gmail.com>
2016-07-15 15:45:55 +0200

    debut chapitre 4

--- src/Chapitre4.scala
+++ src/Chapitre4.scala
@@ -3,4 +3,33 @@
   */
 object Chapitre4 {
 
+  class ChecksumAccumulator{
+    // sum est un field
+    private var sum = 0
+    // sum2 est privé : pas accessible en dehors de la classe
+    var sum2 = 0
+
+    // les parametres d'une methodes sont des vals, par des vars
+    def add(byte: Byte): Unit ={
+      // byte = 1 ne compile pas car byte est une val
+      sum += byte
+    }
+
+    def checksum(): Int ={
+      return ~(sum & 0xFF) + 1
+    }
+  }
+
+
+  def main(args: Array[String]) {
+    val csa = new ChecksumAccumulator
+    val acc = new ChecksumAccumulator
+    acc.sum2 = 3
+    // acc.sum = 0 ne compile pas ! car sum2 est private
+
+    println(acc.checksum())
+  }
+ 
+
+
 }
