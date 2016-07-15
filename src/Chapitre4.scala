b8cfae0f66c005bfae360920da85381cdb256883
zaroursamy <samy.zarour8430@gmail.com>
2016-07-15 16:04:09 +0200

    4.2 p108

--- src/Chapitre4.scala
+++ src/Chapitre4.scala
@@ -10,14 +10,20 @@ object Chapitre4 {
     var sum2 = 0
 
     // les parametres d'une methodes sont des vals, par des vars
-    def add(byte: Byte): Unit ={
+    // méthode a effet de bord: on reassigne sum
+    // le compiler scala peut transformer tout type en Unit. si la méthod renvoie un string, alors il convertit le string en unit (si la methode est declaree unit)
+    def add_ex(byte: Byte): Unit = sum+= byte
       // byte = 1 ne compile pas car byte est une val
-      sum += byte
-    }
 
-    def checksum(): Int ={
-      return ~(sum & 0xFF) + 1
-    }
+    // autre maniere pour definir une methode a effet de bord:
+    // de type Unit egalement
+    def add_ex2(byte: Byte){sum+=byte}
+
+    // en mettant le signe = , le type est retourné (Int ici)
+    def add_ex3(byte: Byte) = sum+=byte
+
+    def checksum(): Int = ~(sum & 0xFF) + 1
+
   }
 
 
