12d6b786837ca7e05397eedf2c11d1146f740cb1
zaroursamy <samy.zarour8430@gmail.com>
2016-07-17 16:43:05 +0200

    fin chapitre 6

--- src/Chapitre6.scala
+++ src/Chapitre6.scala
@@ -5,6 +5,9 @@ object Chapitre6 {
 
   // seul le constructeur primaire peut invoquer un constructeur d'une superclasse
   class Rationnal(n: Int, d: Int){
+
+
+
     // declenche une exception
     require(d != 0)
 
@@ -16,6 +19,9 @@ object Chapitre6 {
 
     // constructeur auxiliaire
     def this(n: Int) = this(n, 1)
+
+    def * (x: Rationnal): Rationnal = new Rationnal(this.n1*x.n1, this.d1*x.d1)
+    def * (n: Int): Rationnal = new Rationnal(this.n1*n, this.d1)
 
     private def gcd(a:Int, b:Int): Int ={
       if(b == 0) a else gcd(b, a%b)
@@ -40,6 +46,20 @@ object Chapitre6 {
     println(r.lessThan(new Rationnal(3,4)))
     println(r.lessThan(new Rationnal(1,3)))
     println(r.max(r2))
+
+    // identifiers
+    val name_ : Int = 1
+    // convention scala des constantes : premiere lettre majuscule
+    val Uneconstante = 3
+
+    val myvar_= = 2
+
+    println(r*r2)
+    println(r*2)
+
+    // pour appeler (2).*r , il faut une conversion d'un entier vers un rationnel
+    implicit def intToRationnal(x: Int): Rationnal = new Rationnal(x)
+    println(2*r)
   }
 
 
