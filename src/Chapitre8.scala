71613f4ffca013ccc5139dc3099d4c1c8bd99dfa
zaroursamy <samy.zarour8430@gmail.com>
2016-07-23 17:16:36 +0200

    p193

--- src/Chapitre8.scala
+++ src/Chapitre8.scala
@@ -21,23 +21,41 @@ object Chapitre8 {
     }
 
 
-    def main(args: Array[String]) {
-      val width = args(0).toInt
-      for(x <- args.drop(1)) FindLongLines.processFile(x, width)
-    }
+//    def main(args: Array[String]) {
+//      val width = args(0).toInt
+//      for(x <- args.drop(1)) processFile(x, width)
+//    }
 
   }
 
 
   def main(args: Array[String]): Unit = {
-    // function litteral
-    (x: Int) => x+1
 
-      // function values (sont des objets)
-      var increase = (x: Int) => x+1
-      println(increase(3))
+    println("ok")
 
+    // function values (sont des objets)
+    var increase = (x: Int) => x+1
+    println(increase(3))
+    // sur plusieurs lignes
+    increase = (x:Int) => {
+      var i = 2
+      x+i
 
+    }
+
+    List(1,10,100,1000).foreach((x:Int) => println(x*2))
+    (1 to 100).filter(_%2==0).foreach(println _)
+
+    val addition = (_: Int)+(_: Int)
+    val carre4 = addition(2,2)
+    println(carre4)
+
+    def somme (a: Int, b:Int, c:Int)= a+b+c
+    println(somme(1,1,1))
+
+    // function value
+    val a = somme _
+    println(a(10,10,10))
 
   }
 }
