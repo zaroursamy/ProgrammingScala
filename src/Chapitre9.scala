a6d4241dcdc7657d7f4610c4e721e9b4d8136354
zaroursamy <samy.zarour8430@gmail.com>
2016-07-25 23:35:20 +0200

    filesMatching ok

--- src/Chapitre9.scala
+++ src/Chapitre9.scala
@@ -9,11 +9,30 @@ object Chapitre9 {
     for(files <- filesHere; if files.getName.endsWith(query)) yield files
   }
 
+  def filesContaining(query: String) = {
+    for(files <- filesHere; if files.getName.contains(query)) yield files
+  }
+
+  def filesRegex(query: String) = {
+    for(files <- filesHere; if files.getName.matches(query)) yield files
+  }
+
+  // on souhaite généraliser maintenant, en passant nimporte quelle methode en parametre
+  def filesMatching (query: String, matcher: (String, String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName, query)) yield file
+  }
 
   def main(args: Array[String]): Unit = {
 
     // higher order function : fonction qui prend en parametre des fonctions
 
     filesEnding(".scala").foreach(println(_))
+    println("containing")
+    filesContaining("9").foreach(println(_))
+    println("regex")
+    filesRegex(".*scala*").foreach(println(_))
+    println("matching")
+    filesMatching("3", (x,y) => x.contains(y)).foreach(println)
+    filesMatching("Chapitre9.scala", _.endsWith(_)).foreach(println)
   }
 }
