c99f042ea6597abfe04448f0bece216ac61a7166
zaroursamy <samy.zarour8430@gmail.com>
2016-07-26 21:06:21 +0200

    p213

--- src/Chapitre9.scala
+++ src/Chapitre9.scala
@@ -5,34 +5,32 @@ object Chapitre9 {
 
   private def filesHere = (new java.io.File("C:\\Users\\Samy\\IdeaProjects\\ProgrammingScala\\src")).listFiles
 
-  def filesEnding(query: String) = {
-    for(files <- filesHere; if files.getName.endsWith(query)) yield files
+  def filesMatching (matcher: (String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName)) yield file
   }
 
-  def filesContaining(query: String) = {
-    for(files <- filesHere; if files.getName.contains(query)) yield files
-  }
+  def filesEnding(query: String) = filesMatching( _.endsWith(query)) // closure car query est exterieur
 
-  def filesRegex(query: String) = {
-    for(files <- filesHere; if files.getName.matches(query)) yield files
-  }
+  def filesContaining(query: String) = filesMatching(_.contains(query))
+
+  def filesRegex(query: String) = filesMatching(_.matches(query))
 
   // on souhaite généraliser maintenant, en passant nimporte quelle methode en parametre
-  def filesMatching (query: String, matcher: (String, String) => Boolean) = {
-    for (file <- filesHere; if matcher(file.getName, query)) yield file
-  }
+
 
   def main(args: Array[String]): Unit = {
 
-    // higher order function : fonction qui prend en parametre des fonctions
+    // higher order function : fonction qui prend en parametre des fonctions. Permet d'eviter la duplication de code
 
     filesEnding(".scala").foreach(println(_))
     println("containing")
     filesContaining("9").foreach(println(_))
     println("regex")
     filesRegex(".*scala*").foreach(println(_))
-    println("matching")
-    filesMatching("3", (x,y) => x.contains(y)).foreach(println)
-    filesMatching("Chapitre9.scala", _.endsWith(_)).foreach(println)
+    println()
+    //println("matching")
+    //filesMatching("3", (x,y) => x.contains(y)).foreach(println)
+    // filesMatching("Chapitre9.scala", _.endsWith(_)).foreach(println)
+    println(List(1,2,3,-2,4,8,-6).exists(_<0))
   }
 }
