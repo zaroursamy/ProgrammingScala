6155a2df50f2542d72b8cfc6e30c9f6211d7cd52
zaroursamy <samy.zarour8430@gmail.com>
2016-07-29 17:16:10 +0200

    chap10: p226, parameterless

--- src/Chapitre10.scala
+++ src/Chapitre10.scala
@@ -3,4 +3,24 @@
   */
 class Chapitre10 {
 
+  // abstract class: signifie que la classe peut avoir des méthodes non implémentées.
+  // on ne peut pas l'instantcier
+  abstract class Element{
+
+    // pas de liste de parametre ! ( ie def height():Int ) : parameterless method. il faut qu'il n'y ai pas de param et qu'elle ne change pas d'etat des mutables
+
+
+    def contents: Array[String]
+
+   // def height: Int = contents.length
+
+   // def weight: Int = if(height != 0) contents(0).length else 0
+
+
+    // mais on peut les mettre sous forme de var: plus rapide , car pré calculé à l'initialisation de la classe,
+    // au lieu detre calcule a chaque fois que la classe est appelée. MAIS les fields prennent plus de place en mémoire
+
+    val height = contents.length
+    val weight = if(height != 0) contents(0).length else 0
+  }
 }
