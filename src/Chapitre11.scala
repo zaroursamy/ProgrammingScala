5618b5ce284e3f6b34bbb4ecc09572fc3f96797a
zaroursamy <samy.zarour8430@gmail.com>
2016-08-09 13:20:28 +0200

    fin chapitre 11

--- src/Chapitre11.scala
+++ src/Chapitre11.scala
@@ -2,12 +2,16 @@
   * Created by Samy on 07/08/2016.
   */
 object Chapitre11 {
+  def isEqual(x: Int, y: Int) = x==y
+  def isEqual2(x: Any, y: Any) = x==y
+
+  def error(msg: String): Nothing = throw new RuntimeException(msg)
+
+  // Nothing est la sous classe de toutes classes, on peut donc l'utiliser de maniere flexible (Nothing est un sous type de Int)
+  def divide(x: Int, y: Int): Int = if(y != 0) x/y else error("divide by zero")
+
 
   def main(args: Array[String]): Unit = {
-
-    def isEqual(x: Int, y: Int) = x==y
-    def isEqual2(x: Any, y: Any) = x==y
-
     println(isEqual(10,10), isEqual2(10,10))
 
     val x = "abcd" substring 2
@@ -16,5 +20,7 @@ object Chapitre11 {
 
     // en scala, egalite de reference se fait avec eq
     println(x eq y, x ne y)
+
+    //println(divide(1,0))
   }
 }
