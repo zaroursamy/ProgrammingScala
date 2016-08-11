0752f6fe9b7375f3c5768b0efc96da4c6e10dec8
zaroursamy <samy.zarour8430@gmail.com>
2016-08-11 23:30:59 +0200

    p269 debut

--- src/Chapitre12.scala
+++ src/Chapitre12.scala
@@ -81,7 +81,20 @@ object Chapitre12 {
     // la methode doit retourner un Integer, 0 si idem, positif si le receiver est plus grand que le parametre, negatif sinon. Ordered n'implemente pas equals
     def compare(that: Rationnal): Int = this.n1*that.d1 - this.d1*that.n1
 
+  }
 
+  abstract class IntQueue{
+    def get(): Int
+    def put(x: Int)
+  }
+
+  import scala.collection.mutable.ArrayBuffer
+  class BasicIntQueue extends IntQueue{
+    val buf = new ArrayBuffer[Int]
+    def get() = buf.remove(0)
+    def put(x: Int) = buf+=x
+
+    override def toString: String = 
   }
 
   def main(args: Array[String]): Unit = {
@@ -107,10 +120,18 @@ object Chapitre12 {
 //    val rect = new Rectangle(new Point(0,1), new Point(1,0))
 //    println(rect.left, rect.right, rect.width)
 
-      val half = new Rationnal(1,2)
-      val third = new Rationnal(1,3)
-      println(half > third)
-    println(half == third)
+//    val half = new Rationnal(1,2)
+//    val third = new Rationnal(1,3)
+//    println(half > third)
+//    println(half == third)
+
+    val queue = new BasicIntQueue
+    queue.put(10)
+    queue.put(20)
+    queue.put(40)
+    println(queue)
+    val i = queue.get()
+    println(i, queue)
 
 
   }
