85bba14ef80b612c27f7ab60487bb0706efa9a4f
zaroursamy <samy.zarour8430@gmail.com>
2016-08-18 00:05:37 +0200

    debut p284

--- src/Chapitre13.scala
+++ src/Chapitre13.scala
@@ -1,3 +1,5 @@
+import bobsdelights.Fruit
+
 /**
   * Created by Samy on 17/08/2016.
   */
@@ -51,10 +53,36 @@ package bobsrockets {
   }// end bobsrockets.launch
 }// end bobsrockets
 
+
+package bobsdelights{
+
+  abstract class Fruit(
+    val name: String,
+    val color: String
+  )
+
+  object Fruits{
+    object Apple extends Fruit("apple","green")
+    object Strawberry extends Fruit("strawberry","red")
+    object Banana extends Fruit("banana","yellow")
+
+    val menu = List(Apple, Strawberry, Banana)
+  }
+}
 object Chapitre13 {
 
 
   def main(args: Array[String]): Unit = {
 
+    def showFruit1(fruit: Fruit): Unit ={
+      println("a "+fruit.name+" is "+fruit.color)
+    }
+    def showFruit2(fruit: Fruit): Unit ={
+      import fruit._
+      println("a "+name+" is "+color)
+    }
+
+    import bobsdelights.Fruits._
+    showFruit1(new Apple)
   }
 }
