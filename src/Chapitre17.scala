8a98daa0b9f5acf0ee1381e74b1c83e3d2b01a91
zaroursamy <samy.zarour8430@gmail.com>
2016-09-10 20:10:24 +0200

    listes des set p384

--- src/Chapitre17.scala
+++ src/Chapitre17.scala
@@ -26,12 +26,53 @@ object Chapitre17 {
 
   def hasMajuscule(x: String): Boolean = x.exists(_.isUpper)
 
+  // Set et Map. scala.collection.immutable.Map = Predef.Map = Map
+  import scala.collection.mutable
+  val mutaSet = mutable.Set(1,2,3)
+  val immuSet = Set(1,2,3)
+
+  val text = "See Spot run. Run, Spot. Run!"
+  val wordsArray = text.split("[ !.,?]+")
+  // methode empty de l'objet compagnon Set
+  val words = mutable.Set.empty[String]
+
+  for(mot <- wordsArray) words+=mot.toLowerCase
+
+  val map = mutable.Map.empty[String, Int]
+
+  // les Set
+  val num = Set(1,2,3)
+  val numM = mutable.Set(1,2,3)
+  // ne modifie pas les Set
+  num +4
+  numM +4
+
+  val music = mutable.Set.empty[String]
+  music ++= List("do", "re", "mi")
+
   def main(args: Array[String]): Unit = {
 
     tableau.foreach(a => print(a +" "))
     println(buf(1))
     println(aBuf, aBuf(0))
     println(hasMajuscule("Salut")) // conversion implicite de string vers stringops car string n'a pas de methode exists. traite le string en sequence de char
+    println(wordsArray.mkString("-"))
+    println(words)
 
+    // methodes sur les Set
+    println(num, numM)
+    println(num+8, numM+10)
+    println(num-1, numM-2)
+    println(Set("a","b")+"c"-"b")
+    println(num -- List(1,3), num ++ List(1,6))
+    println(num & Set(1,3,8,10)) // prend l'intersection des deux Set
+    println(num.size)
+    println(num.contains(9), num.contains(1))
+    println(music)
+    music --= List("do","la","mi")
+    println(music)
+    music.clear()
+    println(music)
+    
   }
 }
