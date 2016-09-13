3ca0b456f2eb577402b3bf04c158b96582fb5b9b
zaroursamy <samy.zarour8430@gmail.com>
2016-09-13 23:40:20 +0200

    19.2 p426

--- src/Chapitre19.scala
+++ src/Chapitre19.scala
@@ -21,15 +21,25 @@ class SlowHeadQueue[T](elemsReversed: List[T]){
 }
 
 // combinaison des deux classes grâce a deux listes
+// asymptotiquement, les queues fonctionnelles sont aussi efficaces que les queues mutables
 class Queue2[T](private val leading: List[T], private val trailing: List[T]){
+
+  // O(n) si leading est empty, sinon O(1)
   private def mirror = if(leading.isEmpty) new Queue2(trailing.reverse, Nil) else this
 
   def head = mirror.leading.head
 
+  // si leading est vide, le tail inverse la fin
   def tail = {
     val q = mirror
     new Queue2(q.leading.tail, q.trailing)
   }
+
+  // le x n'est pas vraiment à la fin ...
+  def enqueue(x: T) = new Queue2(leading, x::trailing) // O(1)
+
+  // le x est a la fin mais on perd la complexité constante
+  def enqueue2(x: T) = new Queue2(leading, trailing:::List[T](x)) // O(n) ...
 
   override def toString: String = leading.mkString(" ")+" | "+trailing.mkString(" ")
 }
@@ -44,6 +54,7 @@ object Chapitre19 {
   val saq = new SlowAppendQueue[Int](List(10,20,55))
 
   val q2 = new Queue2[Int](Nil, List(1,2,3))
+  val q3 = new Queue2[Int](List(1,2,3), List(7,8,9))
 
   def main(args: Array[String]): Unit = {
     println(q, q1)
@@ -51,5 +62,8 @@ object Chapitre19 {
     val saq1 = saq enqueue 33
     println("saq: ",saq, "saq1: ",saq1)
     println(q2.tail)
+    val q3bis = q3 enqueue 22
+    val q3bisbis = q3 enqueue2 22
+    println("q3bis: ", q3bis, "      ", "q3: ", q3, "       ", "q3bisbis: ", q3bisbis)
   }
 }
